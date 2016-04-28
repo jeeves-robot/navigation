1ca222aa235a004b86de00bf493d158785fa667a
Mallika Mathur <mkmathur@cs.uw.edu>
2016-04-28 14:58:43 -0700

    we think we've finished part 10

--- scripts/odometry.py
+++ scripts/odometry.py
@@ -25,28 +25,35 @@ class Tracker:
 
         self._marker_publisher = rospy.Publisher('visualization_marker', Marker)
         self._last_position = Point()
+        self._point_list = list()
+        #self._point_list.append(Point(0,0,0))
 
     def show_text_in_rviz(self, text):
         # We show this relative to base_link
         # If we place the marker relative to the displaced_position,
         #   it will be near where the robot currently is.
         # That's why the pose is very close to 0, instead of a displacement
+        print 'putting down a marker'
         marker = Marker(type=Marker.LINE_STRIP, id=0,
                     lifetime=rospy.Duration(1.5),
-                    pose=Pose(Point(0.5, 0.5, 1.45), Quaternion(0,0,0,1)),
+                    pose=Pose(Point(0, 0, 0), Quaternion(0,0,0,1)),
                     scale=Vector3(0.06, 0.06, 0.06),
-                    header=Header(frame_id=displaced_position),
-                    color=ColorRGBA(0.0, 1.0, 0.0, 0.8), text=text)
+                    header=Header(frame_id=base_position),
+                    color=ColorRGBA(0.0, 1.0, 0.0, 0.8), text=text,
+                    points=self._point_list)
         self._marker_publisher.publish(marker)
 
     def distance(self, p1, p2):
         return ((p1.x - p2.x)**2 + (p1.y - p2.y)**2 + (p1.z - p2.z)**2) ** 0.5
 
     def odom_callback(self, pos):
+
         pointPosition = Point(pos[0], pos[1], pos[2])
+
+        print self.distance(pointPosition, self._last_position)
         if(self.distance(pointPosition, self._last_position) > 0.1):
-            print self.distance(pointPosition, self._last_position)
-            #self._last_position = pointPosition
+            self._last_position = pointPosition
+            self._point_list.append(pointPosition)
             self.show_text_in_rviz("marker")
 
     def run(self):
@@ -58,7 +65,6 @@ class Tracker:
                 self._listener.waitForTransform(base_position, displaced_position, now, rospy.Duration(20.0))
                 (pos, quat) = self._listener.lookupTransform(base_position, displaced_position, rospy.Time(0))
                 self.odom_callback(pos)
-                self._last_position = Point(pos[0], pos[1], pos[2])
                 time.sleep(1)
 
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
